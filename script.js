9d1d2ed1b11d94c91478d2b4f7e0d9b3d360f689
Julie <julie.albini@edu.univ-fcomte.fr>
2025-01-10 14:36:02 +0100

    Javascript navigation development

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Script chargé');
 });
+
+var navigation = document.querySelector("#navigation");
+
+document.querySelector("#menu").addEventListener('click', () => {
+    console.log('coucou')
+    if(navigation.style.display=="block"){
+            navigation.style.display="none";
+        }else{
+            navigation.style.display="block";
+        }
+});
